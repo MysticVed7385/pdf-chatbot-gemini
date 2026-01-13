c19654b60b1a72f8956dfead713bdf8dd23439dd
VedantHage <vedanthage7796@gmail.com>
2026-01-13 14:29:20 +0530

    Fix LangChain text splitter import for new version

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from PyPDF2 import PdfReader
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import (
     GoogleGenerativeAIEmbeddings,
